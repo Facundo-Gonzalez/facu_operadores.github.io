653e69b223dbc11b626e662349e9182f9ac6338e
Facundo-Gonzalez <facu.a.gonzalez646@gmail.com>
2024-06-30 11:39:41 -0400

    commit pages

---
+++ java1.js
@@ -0,0 +1,12 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let mayor = num1;
+if(mayor < num2){
+    mayor = num2;
+}
+    if(mayor < num3){
+        mayor = num3;
+    }
+
+console.log("El mayor es: " + mayor)
